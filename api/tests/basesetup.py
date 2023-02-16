1d8bb9416580f40abff64d85178537e6c014ac62
ndestrieux <n.destrieux@gmail.com>
2023-02-16 11:53:10 +0100

    Base test setup modified: - instance models are not created anymore in setup to avoid signals being triggered when we do not want - fake image added in setup - tear down added to avoid test images kept in media root folder

--- api/tests/basesetup.py
+++ api/tests/basesetup.py
@@ -1,29 +1,38 @@
-from api.models import Profile, ThumbnailSize, User
+from django.core.files.base import ContentFile
+
+from api.models import Image, Thumbnail
 from api.properties import UserTypeChoices
 
 
 class BaseTestSetup:
     def setUp(self) -> None:
-        self.test_basic_user = User.objects.create_user(
-            username="basicuser",
-            password="password",
-            type=UserTypeChoices.BASIC.name,
-        )
-        self.test_premium_user = User.objects.create_user(
-            username="premiumuser",
-            password="password",
-            type=UserTypeChoices.PREMIUM.name,
-        )
-        self.test_enterprise_user = User.objects.create_user(
-            username="enterpriseuser",
-            password="password",
-            type=UserTypeChoices.ENTERPRISE.name,
-        )
-        self.test_custom_user = User.objects.create_user(
-            username="customuser",
-            password="password",
-            type=UserTypeChoices.CUSTOM.name,
-        )
+        self.test_basic_user = {
+            "username": "basicuser",
+            "password": "password",
+            "type": UserTypeChoices.BASIC.name,
+        }
+        self.test_premium_user = {
+            "username": "premiumuser",
+            "password": "password",
+            "type": UserTypeChoices.PREMIUM.name,
+        }
+        self.test_enterprise_user = {
+            "username": "enterpriseuser",
+            "password": "password",
+            "type": UserTypeChoices.ENTERPRISE.name,
+        }
+        self.test_custom_user = {
+            "username": "customuser",
+            "password": "password",
+            "type": UserTypeChoices.CUSTOM.name,
+        }
+        self.testing_image = ContentFile(b"...", name="test.png")
 
     def tearDown(self) -> None:
-        pass
+        images = Image.objects.all()
+        thumbnails = Thumbnail.objects.all()
+        for img in images:
+            img.original.delete()
+            img.binary.delete()
+        for thumb in thumbnails:
+            thumb.thumbnail.delete()
