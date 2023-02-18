f8dfc4bb8244a9392bb2c0b7bd2e96bd4c9cb008
ndestrieux <n.destrieux@gmail.com>
2023-02-18 21:04:09 +0100

    Tests adapted following creation of proxy models.

--- api/tests/test_signals/test_user.py
+++ api/tests/test_signals/test_user.py
@@ -1,17 +1,17 @@
 from django.test import TestCase
 
-from api.models import Profile, ThumbnailSize, User
+from api.models import Profile, RegularUser, ThumbnailSize
 from api.tests.basesetup import BaseTestSetup
 
 
 class UserSignalTest(BaseTestSetup, TestCase):
     def testShouldReturnTrueWhenProfileCreatedAlongWithUser(self):
-        new_user = User.objects.create_user(**self.test_basic_user)
+        new_user = RegularUser.objects.create_user(**self.test_basic_user)
         user_id_list = Profile.objects.all().values_list("user", flat=True)
         self.assertIn(new_user.id, user_id_list)
 
     def testShouldReturnTrueWhenProfileCreatedProperlyForBasicUser(self):
-        new_basic_user = User.objects.create_user(**self.test_basic_user)
+        new_basic_user = RegularUser.objects.create_user(**self.test_basic_user)
         basic_user_profile = Profile.objects.get(user=new_basic_user.id)
         thumbnail_sizes = list(
             ThumbnailSize.objects.filter(profile=basic_user_profile.id).values_list(
@@ -23,7 +23,7 @@ class UserSignalTest(BaseTestSetup, TestCase):
         self.assertEqual(thumbnail_sizes, [200])
 
     def testShouldReturnTrueWhenProfileCreatedProperlyForPremiumUser(self):
-        new_premium_user = User.objects.create_user(**self.test_premium_user)
+        new_premium_user = RegularUser.objects.create_user(**self.test_premium_user)
         premium_user_profile = Profile.objects.get(user=new_premium_user.id)
         thumbnail_sizes = list(
             ThumbnailSize.objects.filter(profile=premium_user_profile.id).values_list(
@@ -35,7 +35,9 @@ class UserSignalTest(BaseTestSetup, TestCase):
         self.assertEqual(thumbnail_sizes, [200, 400])
 
     def testShouldReturnTrueWhenProfileCreatedProperlyForEnterpriseUser(self):
-        new_enterprise_user = User.objects.create_user(**self.test_enterprise_user)
+        new_enterprise_user = RegularUser.objects.create_user(
+            **self.test_enterprise_user
+        )
         enterprise_user_profile = Profile.objects.get(user=new_enterprise_user.id)
         thumbnail_sizes = list(
             ThumbnailSize.objects.filter(
@@ -45,15 +47,3 @@ class UserSignalTest(BaseTestSetup, TestCase):
         self.assertEqual(enterprise_user_profile.original_image_access, True)
         self.assertEqual(enterprise_user_profile.binary_image_access, True)
         self.assertEqual(thumbnail_sizes, [200, 400])
-
-    def testShouldReturnTrueWhenProfileCreatedProperlyForCustomUser(self):
-        new_custom_user = User.objects.create_user(**self.test_custom_user)
-        custom_user_profile = Profile.objects.get(user=new_custom_user.id)
-        thumbnail_sizes = list(
-            ThumbnailSize.objects.filter(profile=custom_user_profile.id).values_list(
-                "size", flat=True
-            )
-        )
-        self.assertEqual(custom_user_profile.original_image_access, False)
-        self.assertEqual(custom_user_profile.binary_image_access, False)
-        self.assertEqual(thumbnail_sizes, [])
