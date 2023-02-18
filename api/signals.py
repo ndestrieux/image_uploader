8aaf3c6e696470f3f062b1e709a13d2495aa15b7
ndestrieux <n.destrieux@gmail.com>
2023-02-18 20:18:58 +0100

    Signals adapted according to the proxies created for User model as well as the way profile properties were reorganized.

--- api/signals.py
+++ api/signals.py
@@ -1,21 +1,24 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from api.models import Image, Profile, ThumbnailSize, User
-from api.properties import (binary_image_properties, original_image_properties,
-                            thumbnail_size_properties)
+from api.models import Image, Profile, RegularUser, ThumbnailSize, User
+from api.properties import profile_properties
 from api.tasks import create_binary_image, create_thumbnail
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=RegularUser)
 def create_profile(sender, instance, created, **kwargs):
-    if created and instance.type:
+    if created:
         profile = Profile.objects.create(
-            original_image_access=original_image_properties[instance.type],
-            binary_image_access=binary_image_properties[instance.type],
+            original_image_access=profile_properties[instance.type][
+                "original_image_access"
+            ],
+            binary_image_access=profile_properties[instance.type][
+                "binary_image_access"
+            ],
             user_id=instance.id,
         )
-        for size in thumbnail_size_properties[instance.type]:
+        for size in profile_properties[instance.type]["thumbnail_sizes"]:
             ThumbnailSize.objects.create(size=size, profile=profile)
 
 
