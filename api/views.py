7e5e471b6ad1e5b253aec658d7e95c8d7937af2e
ndestrieux <n.destrieux@gmail.com>
2023-02-19 16:00:38 +0100

    binary_expire_time field handling added to UploadImageView as per user type.

--- api/views.py
+++ api/views.py
@@ -6,6 +6,9 @@ from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 
 
+from rest_framework.serializers import ValidationError
+from datetime import datetime as dt, timedelta
+
 from api.models import Image
 from api.serializers import ImageListSerializer, UploadImageSerializer
 
@@ -18,6 +21,14 @@ class UploadImageView(CreateAPIView):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        user_profile = Profile.objects.get(user=self.get_user())
+        if serializer.validated_data.get("binary_expire_time", None) and user_profile.binary_image_access:
+            binary_expire_time = serializer.validated_data.pop("binary_expire_time", None)
+            serializer.validated_data["binary_expiration_date"] = dt.now() + timedelta(seconds=binary_expire_time)
+        elif user_profile.binary_image_access:
+            raise ValidationError("binary_expire_time field expected.")
+        else:
+            raise ValidationError("binary_expire_time field unexpected.")
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         data = {
