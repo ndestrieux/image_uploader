778b77f84d72368637b68141741575cb97b37a61
ndestrieux <n.destrieux@gmail.com>
2023-02-19 16:01:32 +0100

    Image display views added.

--- api/views.py
+++ api/views.py
@@ -1,30 +1,40 @@
+from datetime import datetime as dt
+from datetime import timedelta
+
+import pytz
+from django.http import Http404
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
-
-
+from rest_framework.response import Response
 from rest_framework.serializers import ValidationError
-from datetime import datetime as dt, timedelta
 
-from api.models import Image
+from api.models import Image, Profile, Thumbnail
+from api.renderers import JPEGRenderer, PNGRenderer
 from api.serializers import ImageListSerializer, UploadImageSerializer
 
 
 class UploadImageView(CreateAPIView):
     serializer_class = UploadImageSerializer
     permission_classes = (IsAuthenticated,)
-    renderer_classes = (JSONRenderer, )
+    renderer_classes = (JSONRenderer,)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user_profile = Profile.objects.get(user=self.get_user())
-        if serializer.validated_data.get("binary_expire_time", None) and user_profile.binary_image_access:
-            binary_expire_time = serializer.validated_data.pop("binary_expire_time", None)
-            serializer.validated_data["binary_expiration_date"] = dt.now() + timedelta(seconds=binary_expire_time)
+        if (
+            serializer.validated_data.get("binary_expire_time", None)
+            and user_profile.binary_image_access
+        ):
+            binary_expire_time = serializer.validated_data.pop(
+                "binary_expire_time", None
+            )
+            serializer.validated_data["binary_expiration_date"] = dt.now() + timedelta(
+                seconds=binary_expire_time
+            )
         elif user_profile.binary_image_access:
             raise ValidationError("binary_expire_time field expected.")
         else:
@@ -49,7 +59,7 @@ class UploadImageView(CreateAPIView):
 class ImageListAPIView(ListAPIView):
     serializer_class = ImageListSerializer
     permission_classes = (IsAuthenticated,)
-    renderer_classes = (JSONRenderer, )
+    renderer_classes = (JSONRenderer,)
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
@@ -62,3 +72,52 @@ class ImageListAPIView(ListAPIView):
     def get_user(self):
         key = self.request.META.get("HTTP_AUTHORIZATION").split()[-1]
         return Token.objects.get(key=key).user
+
+
+class OriginalImageView(RetrieveAPIView):
+    renderer_classes = (
+        PNGRenderer,
+        JPEGRenderer,
+    )
+    queryset = Image.objects.all()
+    lookup_field = "pk"
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        uploader_profile = Profile.objects.get(user=instance.uploaded_by)
+        if not uploader_profile.original_image_access:
+            raise Http404
+        data = instance.original
+        return Response(data)
+
+
+class BinaryImageView(RetrieveAPIView):
+    renderer_classes = (
+        PNGRenderer,
+        JPEGRenderer,
+    )
+    queryset = Image.objects.all()
+    lookup_field = "pk"
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.binary_expiration_date.replace(tzinfo=pytz.utc) < dt.now().replace(
+            tzinfo=pytz.utc
+        ):
+            raise Http404
+        data = instance.binary
+        return Response(data)
+
+
+class ThumbnailView(RetrieveAPIView):
+    renderer_classes = (
+        PNGRenderer,
+        JPEGRenderer,
+    )
+    queryset = Thumbnail.objects.all()
+    lookup_field = "pk"
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        data = instance.thumbnail
+        return Response(data)
--- config/urls.py
+++ config/urls.py
@@ -17,11 +17,15 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.authtoken.views import ObtainAuthToken
 
-from api.views import ImageListAPIView, UploadImageView
+from api.views import (BinaryImageView, ImageListAPIView, OriginalImageView,
+                       ThumbnailView, UploadImageView)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("authtoken/", ObtainAuthToken.as_view(), name="auth-token"),
     path("upload/", UploadImageView.as_view(), name="upload"),
     path("image_list/", ImageListAPIView.as_view(), name="image-list"),
+    path("image_list/image/<int:pk>/", OriginalImageView.as_view(), name="image"),
+    path("image_list/binary/<int:pk>/", BinaryImageView.as_view(), name="binary"),
+    path("image_list/thumbnail/<int:pk>/", ThumbnailView.as_view(), name="thumbnail"),
 ]
