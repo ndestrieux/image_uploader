4e3d0e566782155aee91f86c3fd9bc9278103569
ndestrieux <n.destrieux@gmail.com>
2023-02-20 15:46:52 +0100

    Tests for models modified: - all usernames start with the word "test" for better handling image deletion once the tests are completed - base setup for models created separately.

--- api/tests/test_models/test_image.py
+++ api/tests/test_models/test_image.py
@@ -2,15 +2,16 @@ from django.db.models.signals import post_save
 from django.test import TestCase
 from factory.django import mute_signals
 
-from api.models import Image, User
-from api.tests.basesetup import BaseTestSetup
+from api.models import Image, RegularUser
+from api.tests.test_models.modelsbasesetup import BaseTestSetup
 
 
 class ImageModelTest(BaseTestSetup, TestCase):
     @mute_signals(post_save)
     def testShouldReturnTrueWhenImageInstanceCreatedInDatabase(self):
-        new_user = User.objects.create_user(**self.test_basic_user)
+        new_user = RegularUser.objects.create_user(**self.test_basic_user)
         new_img = Image.objects.create(
+            name="test_image",
             original=self.testing_image,
             uploaded_by=new_user,
         )
--- api/tests/test_models/test_thumbnailsize.py
+++ api/tests/test_models/test_thumbnailsize.py
@@ -2,14 +2,14 @@ from django.db.models.signals import post_save
 from django.test import TestCase
 from factory.django import mute_signals
 
-from api.models import Profile, ThumbnailSize, User
-from api.tests.basesetup import BaseTestSetup
+from api.models import Profile, RegularUser, ThumbnailSize
+from api.tests.test_models.modelsbasesetup import BaseTestSetup
 
 
 class UserModelTest(BaseTestSetup, TestCase):
     @mute_signals(post_save)
     def testShouldReturnTrueWhenUserInstanceCreatedInDatabase(self):
-        new_user = User.objects.create_user(**self.test_basic_user)
+        new_user = RegularUser.objects.create_user(**self.test_basic_user)
         new_profile = Profile.objects.create(user=new_user)
         thumbnail_sizes = ThumbnailSize.objects.all()
         new_tn_size = ThumbnailSize.objects.create(size=200, profile=new_profile)
